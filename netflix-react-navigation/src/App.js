62b9e6941e3ad2dc78aca5367378fc6c851f8b28
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-10-06 16:31:04 +0200

    added Route path and ShowDetail comp

--- netflix-react-navigation/src/App.js
+++ netflix-react-navigation/src/App.js
@@ -2,16 +2,17 @@ import "./App.css";
 import CustomNavbar from "./Component/Navbar";
 import Header from "./Component/Header";
 import RowOfMovies from "./Component/RowOfMovies";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <CustomNavbar />
       <Header />
-      <RowOfMovies title="Star Wars" />
-      <RowOfMovies title="Harry Potter" />
-      <RowOfMovies title="The lord of the rings" />
+      <Route path="/" exact render={(routerProps)=> <RowOfMovies {...routerProps}title="Star Wars" />} />
+      <Route path="/" exact render={(routerProps)=> <RowOfMovies {...routerProps}title="Star Wars" />}/>
+      <Route path="/" exact render={(routerProps)=> <RowOfMovies {...routerProps}title="Star Wars" />}/>
+      <Route path="/details/:movieID" render={(routerProps) => <ShowDetail {...routerProps} title="hello" />} />
     </Router>
   );
 }
