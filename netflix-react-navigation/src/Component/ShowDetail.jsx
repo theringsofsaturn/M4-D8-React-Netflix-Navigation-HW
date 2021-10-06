1552fe162efe72c39d9a841d285ba7f2d6cbc49d
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-10-06 16:45:16 +0200

    fixing error

--- netflix-react-navigation/src/Component/ShowDetail.jsx
+++ netflix-react-navigation/src/Component/ShowDetail.jsx
@@ -1,19 +1,20 @@
 import { useEffect } from "react";
 
-const showDetail = () => {};
-const [info, setInfo] = useState(null);
-const [comments, setComments] = useState([]);
+const showDetail = (match) => {
+  const [info, setInfo] = useState(null);
+  const [comments, setComments] = useState([]);
 
-useEffect(() => {
-  const getMovieData = async () => {
-    let id = match.params.MovieID;
-    if (id) {
-      let response = await fetch(
-        "http://www.omdbapi.com/?i=tt3896198&apikey=c82e0eba" + id
-      );
-      let movieInfo = await response.json();
-      setInfo(movieInfo);
-    }
-  };
-  getMovieData();
-}, [match.params.MovieID]);
+  useEffect(() => {
+    const getMovieData = async () => {
+      let id = match.params.MovieID;
+      if (id) {
+        let response = await fetch(
+          "http://www.omdbapi.com/?i=tt3896198&apikey=c82e0eba" + id
+        );
+        let movieInfo = await response.json();
+        setInfo(movieInfo);
+      }
+    };
+    getMovieData();
+  }, [match.params.MovieID]);
+};
