1b08a79f467ed426b6d3adf803049718fbd179ac
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-10-06 17:09:55 +0200

    fixes

--- netflix-react-navigation/src/Component/MovieDetails.jsx
+++ netflix-react-navigation/src/Component/MovieDetails.jsx
@@ -3,9 +3,9 @@ const MovieDetails = ({ movie, comments }) => (
     <h2>{movie.Title}</h2>
     <img src={movie.Poster} alt="movie poster" />
     <ul>
-      {comments.map((c) => (
-        <li className="my-3" key={c._id}>
-          {c.comment}
+      {comments.map((comment) => (
+        <li className="my-3" key={comment._id}>
+          {comment.comment}
         </li>
       ))}
     </ul>
