547b91ff60a4fb03eca17b84be79231979ff0e81
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-10-06 16:16:07 +0200

    imported react-router-dom

--- netflix-react-navigation/src/App.js
+++ netflix-react-navigation/src/App.js
@@ -1,18 +1,19 @@
-import './App.css'
-import CustomNavbar from './Component/Navbar'
-import Header from './Component/Header'
-import RowOfMovies from './Component/RowOfMovies'
+import "./App.css";
+import CustomNavbar from "./Component/Navbar";
+import Header from "./Component/Header";
+import RowOfMovies from "./Component/RowOfMovies";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <CustomNavbar />
       <Header />
       <RowOfMovies title="Star Wars" />
       <RowOfMovies title="Harry Potter" />
       <RowOfMovies title="The lord of the rings" />
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
